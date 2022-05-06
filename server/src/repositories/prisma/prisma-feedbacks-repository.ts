f84c53aef355a2553a20c94dca47ceca5eb4b043
Lucas Trindade <trindadebra@gmail.com>
2022-05-06 00:27:51 -0300

    mobile feito!

--- server/src/repositories/prisma/prisma-feedbacks-repository.ts
+++ server/src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -2,14 +2,13 @@ import { prisma } from "../../prisma";
 import { FeedbackCreateData, FeedbacksRepository } from "../feedbacks-repository";
 
 export class PrismaFeedbacksRepository implements FeedbacksRepository {
-    async create({ type, comment, screenshot }: FeedbackCreateData) {
-        await prisma.feedback.create({
-            data: {
-                type: type,
-                comment: comment,
-                screenshot: screenshot,
-    
-            }
-        })
-    };
+  async create({ type, comment, screenshot }: FeedbackCreateData) {
+    await prisma.feedback.create({
+      data: {
+        type,
+        comment,
+        screenshot,
+      },
+    });
+  }
 }
